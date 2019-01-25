7ef4ec5a15194ffa49f02827aaaeb7628fd90fb4
Goos van den Bekerom <goos.bekerom@gmail.com>
2019-01-25 16:14:39 +0100

    initial commit

---
+++ src/main/kotlin/com/skatesquare/spotservice/SpotserviceApplication.kt
@@ -0,0 +1,12 @@
+package com.skatesquare.spotservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpotserviceApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/skatesquare/spotservice/SpotserviceApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.skatesquare.spotservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpotserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
+
